369fcd6464df80c0d48b43757b52ddd721f064ab
yynn1215 <yena951215@naver.com>
2019-09-20 15:01:21 +0900

    i2c

---
+++ I2C_TEST2/Src/I2C_1.h
@@ -0,0 +1,21 @@
+/*
+ * I2C_1.h
+ *
+ *  Created on: 2019. 8. 19.
+ *      Author: bitcamp
+ */
+
+#ifndef I2C_1_H_
+#define I2C_1_H_
+
+#include "stm32f4xx_hal.h"
+
+typedef enum _I2C_Result_t {
+    I2C_Result_Ok = 0x00,
+    I2C_Result_Error,
+} I2C_Result_t;
+
+I2C_Result_t I2C_Write(I2C_HandleTypeDef* hi2c, uint8_t device_address, uint8_t register_address, uint8_t data);
+I2C_Result_t I2C_ReadMulti(I2C_HandleTypeDef* hi2c, uint8_t device_address, uint8_t register_address, uint8_t* data, uint16_t count);
+
+#endif /* I2C_1_H_ */
